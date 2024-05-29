3f581146faefc0b8cbd0b1e81ee1ae1b4039091b
TheAstutian <speakingatoms@gmail.com>
2024-05-29 18:03:40 +0100

    add mock db to project

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import cors from'cors'
+
+import { database } from './db.js';
 
 const app = express();
+app.use(cors())
 app.use(express.json())
+
+const sample_db = {
+    one: "sample 1",
+    two: "sample2",
+}
+
+app.get('/', (req,res)=>{
+    res.json(sample_db)
+})
+
+app.get('/products', (req,res)=>{
+    res.json(database)
+})
 
 app.listen(8800, ()=>{
     console.log("Connected!")
